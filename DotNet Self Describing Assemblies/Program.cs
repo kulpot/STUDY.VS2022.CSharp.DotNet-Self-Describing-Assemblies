b69beb1cd92b7e0693178533896a75f040c50e9c
Kulpot <kulpotwahu@gmail.com>
2023-09-24 02:06:55 +0800

    Add project files.

---
+++ DotNet Self Describing Assemblies/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=BQGvT9-HhDI&list=PLRwVmtr-pp06rfSgNYu_oBg40DkwXiRHt&index=12
+//
+
+public class Farm { }
+public struct Cow { }
+public delegate void Donkey();
+public enum Chicken { Eggs }
+public class Bacon { }
